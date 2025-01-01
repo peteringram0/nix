f223dfb6c8b02f18247083d19ebe871059e43289
Peter Ingram <p.ingram@mrx.technology>
2025-01-01 22:02:02 +0000

    yah boi

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -5,68 +5,79 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    
-    # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-
-    # Optional: Declarative tap management
-    # homebrew-core = {
-    #   url = "github:homebrew/homebrew-core";
-    #   flake = false;
-    # };
-    # homebrew-cask = {
-    #   url = "github:homebrew/homebrew-cask";
-    #   flake = false;
-    # };
-    # homebrew-bundle = {
-    #   url = "github:homebrew/homebrew-bundle";
-    #   flake = false;
-    # };
   };
-
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
 
       environment.systemPackages = [
-        pkgs.vim
-        pkgs.helix
+
+        # Generic System
+        pkgs.wget
+        pkgs.obsidian
+        pkgs.discord
+        
         pkgs.ghostty
         pkgs.yazi
-        pkgs.dockutil
-        pkgs.stdenv
+        pkgs.chezmoi
+        pkgs.eza
+        pkgs.bat
+        pkgs.rectangle
+        pkgs.starship
+        pkgs.raycast
+        pkgs.lazygit
+        pkgs.delta
+        pkgs.docker
+        pkgs.ripgrep
+        pkgs.google-chrome
+
+        pkgs.oh-my-zsh
+        pkgs.zsh
+        pkgs.zsh-completions
+       
+        pkgs.cargo
+
+        pkgs.nil
       ];
-      
-      # nix-homebrew = {
-      #   # Install Homebrew under the default prefix
-      #   enable = true;
 
-      #   # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-      #   enableRosetta = true;
-
-      #   # User owning the Homebrew prefix
-      #   # user = "yourname";
-
-      #   # Optional: Declarative tap management
-      #   # taps = {
-      #   #   "homebrew/homebrew-core" = homebrew-core;
-      #   #   "homebrew/homebrew-cask" = homebrew-cask;
-      #   #   "homebrew/homebrew-bundle" = homebrew-bundle;
-      #   # };
-
-      #   # Optional: Enable fully-declarative tap management
-      #   #
-      #   # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-      #   mutableTaps = false;
-      # };
-
+      nixpkgs.config.allowUnfree = true;
       nix.settings.experimental-features = "nix-command flakes";
 
-      imports = [
-       ./dock
-      ];
+      programs.zsh = {
+        enable = true;
+      };
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+      system.defaults = {
+        dock = {
+          autohide = true;
+          persistent-apps = [
+            "/Applications/Nix Apps/Ghostty.app"
+          ];
+          show-recents = false;
+          tilesize = 20;
+          mineffect = null;
+        };
+      };
+
+      homebrew = {
+        enable = true;
+        caskArgs.no_quarantine = true;
+        global.brewfile = true;
+        masApps = { };
+        casks = [ "1password" "whatsapp" ];
+        # taps = [ "fujiapple852/trippy" ];
+        # brews = [ "trippy" ];
+      };
+           
+      # imports = [
+      #  ./dock
+      # ];
+
+      # Scroll direction
+      system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
+
+      # Show hidden files in finder
+      system.defaults.NSGlobalDomain.AppleShowAllFiles = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -76,22 +87,16 @@
       system.stateVersion = 5;
 
       nixpkgs.hostPlatform = "aarch64-darwin";
-
-      # Fully declarative dock using the latest from Nix Store
-      # local = {
-      #   dock.enable = true;
-      #   dock.entries = [
-      #     { path = "/System/Applications/Facetime.app/"; }
-      #   ];
-      # };
-
+      
     };
   in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [
+        configuration
+      ];
     };
   };
 }
