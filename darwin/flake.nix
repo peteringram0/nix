1a239342d9e26a54e4dabbe4c0a30b8bd5d008f5
Peter Ingram <p.ingram@mrx.technology>
2025-01-01 18:03:27 +0000

    a

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -5,6 +5,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    
+    # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    # Optional: Declarative tap management
+    # homebrew-core = {
+    #   url = "github:homebrew/homebrew-core";
+    #   flake = false;
+    # };
+    # homebrew-cask = {
+    #   url = "github:homebrew/homebrew-cask";
+    #   flake = false;
+    # };
+    # homebrew-bundle = {
+    #   url = "github:homebrew/homebrew-bundle";
+    #   flake = false;
+    # };
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
@@ -19,6 +35,29 @@
         pkgs.dockutil
         pkgs.stdenv
       ];
+      
+      # nix-homebrew = {
+      #   # Install Homebrew under the default prefix
+      #   enable = true;
+
+      #   # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+      #   enableRosetta = true;
+
+      #   # User owning the Homebrew prefix
+      #   # user = "yourname";
+
+      #   # Optional: Declarative tap management
+      #   # taps = {
+      #   #   "homebrew/homebrew-core" = homebrew-core;
+      #   #   "homebrew/homebrew-cask" = homebrew-cask;
+      #   #   "homebrew/homebrew-bundle" = homebrew-bundle;
+      #   # };
+
+      #   # Optional: Enable fully-declarative tap management
+      #   #
+      #   # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+      #   mutableTaps = false;
+      # };
 
       nix.settings.experimental-features = "nix-command flakes";
 
