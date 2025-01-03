882fce624c16fed6aebc6b24731b63541e9a5ac1
Peter Ingram <peter.ingram0@gmail.com>
2025-01-03 20:39:09 +0000

    a

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -5,10 +5,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    mac-app-util.url = "github:hraban/mac-app-util";
   };
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, mac-app-util }:
   let
     configuration = { pkgs, ... }: {
+
+      mac-app-util.darwinModules.default
 
       environment.systemPackages = [
 
